0715942ce71a85e14da25ae6221194473e0a4533
Case <90377400+CaseIRL@users.noreply.github.com>
2022-08-27 19:55:38 +0100

    new qb update + refactor
    Added add/remove item events into server side due to recent qb-core update. 
    Removed useless player line

--- server/consumables_s.lua
+++ server/consumables_s.lua
@@ -36,17 +36,14 @@ end)
 -- Store items
 Core.Functions.CreateUseableItem('eggsandwich', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:EatRiskyFood', source, 1, item.name)
 end)
 Core.Functions.CreateUseableItem('tunasandwich', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:EatRiskyFood', source, 2, item.name)
 end)
 Core.Functions.CreateUseableItem('hamsandwich', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:EatRiskyFood', source, 3, item.name)
 end)
 --<!>-- RISKY FOOD ITEMS END --<!>--
@@ -54,17 +51,14 @@ end)
 --<!>-- FOOD ITEMS START --<!>-- 
 Core.Functions.CreateUseableItem('hersheysbar', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:EatFood', source, 1, item.name)
 end)
 Core.Functions.CreateUseableItem('mandms', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:EatFood', source, 2, item.name)
 end)
 Core.Functions.CreateUseableItem('peanutmandms', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:EatFood', source, 3, item.name)
 end)
 --<!>-- RISKY FOOD ITEMS END --<!>--
@@ -72,42 +66,34 @@ end)
 --<!>-- DRINKS START --<!>--
 Core.Functions.CreateUseableItem('cocacola', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 1, item.name)
 end)
 Core.Functions.CreateUseableItem('pepsi', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 2, item.name)
 end)
 Core.Functions.CreateUseableItem('drpepper', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 3, item.name)
 end)
 Core.Functions.CreateUseableItem('mountaindew', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 4, item.name)
 end)
 Core.Functions.CreateUseableItem('water', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 5, item.name)
 end)
 Core.Functions.CreateUseableItem('coffee', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 6, item.name)
 end)
 Core.Functions.CreateUseableItem('tea', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 7, item.name)
 end)
 Core.Functions.CreateUseableItem('hotchocolate', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Drink', source, 8, item.name)
 end)
 --<!>-- DRINKS END --<!>--
@@ -115,12 +101,10 @@ end)
 --<!>-- RISKY DRINKS START --<!>--
 Core.Functions.CreateUseableItem('yellowliquid', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:RiskyDrink', source, 1, item.name)
 end)
 Core.Functions.CreateUseableItem('dirtywater', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:RiskyDrink', source, 2, item.name)
 end)
 --<!>-- RISKY DRINKS END --<!>--
@@ -128,32 +112,26 @@ end)
 --<!>-- ALCOHOL START --<!>--
 Core.Functions.CreateUseableItem('corona', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Alcohol', source, 1, item.name)
 end)
 Core.Functions.CreateUseableItem('corona6pack', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Open6Pack', source, 1)
 end)
 Core.Functions.CreateUseableItem('budweiser', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Alcohol', source, 2, item.name)
 end)
 Core.Functions.CreateUseableItem('budweiser6pack', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Open6Pack', source, 2)
 end)
 Core.Functions.CreateUseableItem('vodka', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Alcohol', source, 3, item.name)
 end)
 Core.Functions.CreateUseableItem('whiskey', function(source, item)
     local source = source
-    local Player = Core.Functions.GetPlayer(source)
     TriggerClientEvent('boii-consumables:client:Alcohol', source, 4, item.name)
 end)
 --<!>-- ALCOHOL END --<!>--
