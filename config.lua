b7567fb25a33182785a9e01706845710736ee4ac
Case <90377400+CaseIRL@users.noreply.github.com>
2022-10-12 13:26:07 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,5 +9,6 @@ Config.CoreSettings = {
     CoreFolder = 'qb-core', -- The name of your core folder. Default; qb-core
     MetaDataEvent = 'QBCore:Server:SetMetaData', -- The name of your set metadata event. Default; 'QBCore:Server:SetMetaData'
     AddStressEvent = 'hud:server:GainStress', -- Event to remove stress from player. Default; 'hud:server:GainStress'
+    RemoveStressEvent = 'hud:server:RelieveStress', -- Event to remove stress from player. Default; 'hud:server:RelieveStress'
     EvidenceEvent = 'evidence:client:SetStatus', -- Event to set drunk evidence status for police. Default; 'evidence:client:SetStatus'
 }
