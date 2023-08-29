96d86628251944990d8cc49116593dc916599771
Unsociable <andrew.cade@outlook.com>
2023-08-29 20:56:57 +0100

    Updated Items to Array
    Updated items to loop through array on load

--- server/items_sv.lua
+++ server/items_sv.lua
@@ -8,136 +8,61 @@ local CoreFolder = Config.CoreSettings.CoreFolder
 local Core = exports[CoreFolder]:GetCoreObject()
 --<!>-- DO NOT EDIT ANYTHING ABOVE THIS TEXT UNLESS YOU KNOW WHAT YOU ARE DOING SUPPORT WILL NOT BE PROVIDED IF YOU IGNORE THIS --<!>--
 
---<!>-- FOOD --<!>--
-Core.Functions.CreateUseableItem('eggsandwich', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:EatFood', source, item.name)
-end)
+local foodItems = {
+    'eggsandwich',
+    'tunasandwich',
+    'hamsandwich',
+    'meteorite',
+    'egochaser',
+    'psnqs'
+}
 
-Core.Functions.CreateUseableItem('tunasandwich', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:EatFood', source, item.name)
-end)
+local drinkItems = {
+    'water',
+    'ecola',
+    'sprunk',
+    'orangotang',
+    'dirtywater'
+}
 
-Core.Functions.CreateUseableItem('hamsandwich', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:EatFood', source, item.name)
-end)
+local alcoholItems = {
+    'beer_am',
+    'beer_logger',
+    'beer_piss',
+    'beer_dusche',
+    'beer_patriot',
+    'beer_stz',
+    'beer_pride',
+    'beer_jakey',
+    'beer_bar',
+    'beer_blr',
+    'vodka',
+    'whiskey_mount',
+    'whiskey_macbeth',
+    'whiskey_richards',
+    'brandy_cardi',
+    'brandy_cognac',
+    'rum',
+    'tequila',
+    'wine_red',
+    'wine_rose',
+    'wine_white'
+}
 
-Core.Functions.CreateUseableItem('meteorite', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:EatFood', source, item.name)
-end)
+for index, item in ipairs(foodItems) do
+    Core.Functions.CreateUseableItem(item, function(source, itemdata)
+        TriggerClientEvent('boii-consumables:cl:EatFood', source, itemdata.name)
+    end)
+end
 
-Core.Functions.CreateUseableItem('egochaser', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:EatFood', source, item.name)
-end)
+for index, item in ipairs(drinkItems) do
+    Core.Functions.CreateUseableItem(item, function(source, itemdata)
+        TriggerClientEvent('boii-consumables:cl:Drink', source, itemdata.name)
+    end)
+end
 
-Core.Functions.CreateUseableItem('psnqs', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:EatFood', source, item.name)
-end)
---<!>-- FOOD --<!>--
-
---<!>-- DRINKS --<!>--
-Core.Functions.CreateUseableItem('water', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Drink', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('ecola', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Drink', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('sprunk', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Drink', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('orangotang', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Drink', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('dirtywater', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Drink', source, item.name)
-end)
---<!>-- DRINKS --<!>--
-
---<!>-- ALCOHOL --<!>--
-Core.Functions.CreateUseableItem('beer_am', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_logger', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_piss', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_dusche', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_patriot', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_stz', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_pride', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_jakey', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_bar', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('beer_blr', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('vodka', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('whiskey_mount', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('whiskey_macbeth', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('whiskey_richards', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('brandy_cardi', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('brandy_cognac', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('rum', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('tequila', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('wine_red', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('wine_rose', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
-
-Core.Functions.CreateUseableItem('wine_white', function(source, item)
-    TriggerClientEvent('boii-consumables:cl:Alcohol', source, item.name)
-end)
---<!>-- ALCOHOL --<!>--
+for index, item in ipairs(alcoholItems) do
+    Core.Functions.CreateUseableItem(item, function(source, itemdata)
+        TriggerClientEvent('boii-consumables:cl:Alcohol', source, itemdata.name)
+    end)
+end
